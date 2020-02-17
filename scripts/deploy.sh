238e3c464b3da7ac82794157c0485a6759201803
Genyus <genyus@gmail.com>
2020-02-17 00:19:37 +0000

    chore: adds CircleCI deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+if [ -z "$1" ]
+    then
+        echo "No branch supplied"
+        exit 1
+fi
+
+branch_name=$1
+env_file="public/.env"
+
+if [ -f "$env_file" ]
+    then
+        echo ".env file found"
+    else
+        echo ".env file not found"
+        exit 1
+fi
+
+echo "Deploying $branch_name"
+cd public/
+git config --global user.email "ENTER EMAIL"
+git config --global user.name "ENTER NAME"
+git fetch --all
+git reset --hard origin/"$branch_name"
+git pull origin "$branch_name"
+git checkout "$branch_name"
+
+echo "TASK: git pull finished"
+composer install --no-dev --optimize-autoloader
+
+echo "TASK: composer install finished"
