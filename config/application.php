786e2ebbe3f77fc7ac186155f9f9140f107f5d3e
Genyus <genyus@gmail.com>
2020-03-05 00:27:23 -0400

    chore: configures project for automated db syncing

--- config/application.php
+++ config/application.php
@@ -51,7 +51,12 @@ define('WP_CONTENT_URL', WP_HOME . CONTENT_DIR);
 define('DB_NAME', env('DB_NAME'));
 define('DB_USER', env('DB_USER'));
 define('DB_PASSWORD', env('DB_PASSWORD'));
-define('DB_HOST', env('DB_HOST') ?: 'localhost');
+if (WP_ENV == 'development' && defined('WP_CLI') && WP_CLI) {
+    define('DB_HOST', env('DB_HOST_LOCAL'));
+}
+else {
+    define('DB_HOST', env('DB_HOST') ?: 'localhost');
+}
 define('DB_CHARSET', 'utf8mb4');
 define('DB_COLLATE', '');
 $table_prefix = env('DB_PREFIX') ?: 'wp_';
