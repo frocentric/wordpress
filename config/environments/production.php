5d72969c75a9a229cf004482f00bc872cc6ee03d
Genyus <genyus@gmail.com>
2020-03-06 10:12:28 -0400

    chore: restores WP Rocket as Composer dependency

--- config/environments/production.php
+++ config/environments/production.php
@@ -5,3 +5,5 @@ define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
+define('WP_ROCKET_EMAIL', env('WP_ROCKET_EMAIL') ?: '');
+define('WP_ROCKET_KEY', env('WP_ROCKET_KEY') ?: '');
