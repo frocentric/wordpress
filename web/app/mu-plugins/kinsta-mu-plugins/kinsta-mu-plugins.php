58090fa4f83b9e0b2c2adbfc3d4efb62aa1f5a31
Genyus <genyus@gmail.com>
2020-03-06 12:57:23 -0400

    chore: installs Kinsta MU plugin manually
    Removes Composer dependency due to packaging issue

---
+++ web/app/mu-plugins/kinsta-mu-plugins/kinsta-mu-plugins.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name: Kinsta Must-use Plugins
+ * Plugin URI: https://kinsta.com/knowledgebase/kinsta-mu-plugin/
+ * Description: The plugin designed to work on Kinsta's managed WordPress hosting platform.
+ * Version: 2.3.3
+ * Author: Kinsta Team
+ * Author URI: https://kinsta.com/about-us/
+ * Text Domain: kinsta-mu-plugins
+ * Domain Path: /kinsta-mu-plugins/shared/translations
+ *
+ * @package KinstaMUPlugins
+ */
+
+if ( ! defined( 'ABSPATH' ) ) { // If this file is called directly.
+	die( 'No script kiddies please!' );
+}
+
+define( 'KINSTAMU_VERSION', '2.3.3' );
+if ( ! defined( 'KINSTAMU_WHITELABEL' ) ) {
+	define( 'KINSTAMU_WHITELABEL', false );
+}
+
+require_once plugin_dir_path( __FILE__ ) . 'kinsta-mu-plugins/shared/class-shared.php';
+require_once plugin_dir_path( __FILE__ ) . 'kinsta-mu-plugins/compat/compat.php';
+require_once plugin_dir_path( __FILE__ ) . 'kinsta-mu-plugins/cache/cache.php';
+require_once plugin_dir_path( __FILE__ ) . 'kinsta-mu-plugins/cdn/cdn.php';
