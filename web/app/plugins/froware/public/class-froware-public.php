2ed5c974f0f305e76849363af2b6229b946e192e
Genyus <genyus@gmail.com>
2020-04-05 01:05:10 +0100

    feature: displays Froware logo on login page

--- web/app/plugins/froware/public/class-froware-public.php
+++ web/app/plugins/froware/public/class-froware-public.php
@@ -79,6 +79,30 @@ class Froware_Public {
 	}
 
 	/**
+	 * Register the stylesheets for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_login_styles() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Froware_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Froware_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/froware-login.css', array(), $this->version, 'all' );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/froware-login.js', array( 'jquery' ), $this->version, false );
+
+	}
+
+	/**
 	 * Register the JavaScript for the public-facing side of the site.
 	 *
 	 * @since    1.0.0
---
+++ web/app/plugins/froware/public/js/froware-login.js
@@ -0,0 +1,7 @@
+(function( $ ) {
+	'use strict';
+	
+	 $(function() {
+		 $('.login h1 a').attr('href', '/');
+	 });
+})( jQuery );
