387d7a38cee00ea31381515826023450f33e6a4d
Genyus <genyus@gmail.com>
2020-07-22 22:51:17 +0100

    style: Prettier reformatting

--- web/app/plugins/froware/public/js/froware-public.js
+++ web/app/plugins/froware/public/js/froware-public.js
@@ -58,7 +58,8 @@
 
             if (response.success && response.data && response.data.ID) {
               window.location =
-                settings.homeurl + "/events/community/edit/event/" +
+                settings.homeurl +
+                "/events/community/edit/event/" +
                 response.data.ID;
             } else {
               $button.prop("value", title).prop("disabled", false);
