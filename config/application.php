8051ef9308cb120097b4b1d598b06b9b9a8a3538
Genyus <genyus@gmail.com>
2020-09-11 03:17:54 +0100

    chore: Ensures logins work on mapped domains

--- config/application.php
+++ config/application.php
@@ -130,6 +130,7 @@ Config::define( 'DOMAIN_CURRENT_SITE', env('DOMAIN_CURRENT_SITE') );
 Config::define( 'PATH_CURRENT_SITE', env('PATH_CURRENT_SITE') ?? '/' );
 Config::define( 'SITE_ID_CURRENT_SITE', env('SITE_ID_CURRENT_SITE') ?? 1 );
 Config::define( 'BLOG_ID_CURRENT_SITE', env('BLOG_ID_CURRENT_SITE') ?? 1 );
+Config::define( 'COOKIE_DOMAIN', $_SERVER['HTTP_HOST'] );
 $base = '/';
 
 /**
