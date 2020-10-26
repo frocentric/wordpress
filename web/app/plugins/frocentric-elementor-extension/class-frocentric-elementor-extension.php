476bc8939a29edcdf37373c5cc60fb0f7a6a4b30
Genyus <genyus@gmail.com>
2020-10-26 02:14:46 +0000

    feat: Creates new blog post list widget for homepage

---
+++ web/app/plugins/frocentric-elementor-extension/class-frocentric-elementor-extension.php
@@ -0,0 +1,320 @@
+<?php
+/**
+ * Custom Elementor extension
+ *
+ * @link              https://frocentric.org
+ * @since             1.0.0
+ * @package           Froware
+ *
+ * Plugin Name: Frocentric Elementor Extension
+ * Description: Custom Elementor extension.
+ * Plugin URI:  https://github.com/frocentric/wordpress
+ * Version:     1.0.0
+ * Author:      Frocentric CIC
+ * Author URI:  https://www.frocentric.org/
+ * Text Domain: frocentric-elementor-extension
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Main Frocentric Elementor Extension Class
+ *
+ * The main class that initiates and runs the plugin.
+ *
+ * @since 1.0.0
+ */
+final class Frocentric_Elementor_Extension {
+
+	/**
+	 * Plugin Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string The plugin version.
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 * Minimum Elementor Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Minimum Elementor version required to run the plugin.
+	 */
+	const MINIMUM_ELEMENTOR_VERSION = '2.0.0';
+
+	/**
+	 * Minimum PHP Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Minimum PHP version required to run the plugin.
+	 */
+	const MINIMUM_PHP_VERSION = '7.0';
+
+	/**
+	 * Instance
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access private
+	 * @static
+	 *
+	 * @var Frocentric_Elementor_Extension The single instance of the class.
+	 */
+	private static $instance = null;
+
+	/**
+	 * Instance
+	 *
+	 * Ensures only one instance of the class is loaded or can be loaded.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 * @static
+	 *
+	 * @return Frocentric_Elementor_Extension An instance of the class.
+	 */
+	public static function instance() {
+
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+
+	}
+
+	/**
+	 * Constructor
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function __construct() {
+
+		add_action( 'plugins_loaded', array( $this, 'on_plugins_loaded' ) );
+
+	}
+
+	/**
+	 * Load Textdomain
+	 *
+	 * Load plugin localization files.
+	 *
+	 * Fired by `init` action hook.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function i18n() {
+
+		load_plugin_textdomain( 'elementor-test-extension' );
+
+	}
+
+	/**
+	 * On Plugins Loaded
+	 *
+	 * Checks if Elementor has loaded, and performs some compatibility checks.
+	 * If All checks pass, inits the plugin.
+	 *
+	 * Fired by `plugins_loaded` action hook.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function on_plugins_loaded() {
+
+		if ( $this->is_compatible() ) {
+			add_action( 'elementor/init', array( $this, 'init' ) );
+		}
+
+	}
+
+	/**
+	 * Compatibility Checks
+	 *
+	 * Checks if the installed version of Elementor meets the plugin's minimum requirement.
+	 * Checks if the installed PHP version meets the plugin's minimum requirement.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function is_compatible() {
+
+		// Check if Elementor installed and activated.
+		if ( ! did_action( 'elementor/loaded' ) ) {
+			add_action( 'admin_notices', array( $this, 'admin_notice_missing_main_plugin' ) );
+			return false;
+		}
+
+		// Check for required Elementor version.
+		if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
+			add_action( 'admin_notices', array( $this, 'admin_notice_minimum_elementor_version' ) );
+			return false;
+		}
+
+		// Check for required PHP version.
+		if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
+			add_action( 'admin_notices', array( $this, 'admin_notice_minimum_php_version' ) );
+			return false;
+		}
+
+		return true;
+
+	}
+
+	/**
+	 * Initialize the plugin
+	 *
+	 * Load the plugin only after Elementor (and other plugins) are loaded.
+	 * Load the files required to run the plugin.
+	 *
+	 * Fired by `plugins_loaded` action hook.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function init() {
+
+		$this->i18n();
+
+		// Add Plugin actions.
+		add_action( 'elementor/widgets/widgets_registered', array( $this, 'init_widgets' ) );
+		add_action( 'elementor/controls/controls_registered', array( $this, 'init_controls' ) );
+
+	}
+
+	/**
+	 * Init Widgets
+	 *
+	 * Include widgets files and register them
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function init_widgets() {
+
+		// Include Widget files.
+		require_once __DIR__ . '/widgets/class-post-grid-widget.php';
+
+		$post_grid_widget = new \Post_Grid_Widget();
+
+		// Register widget.
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( $post_grid_widget );
+		add_filter( 'generate_blog_columns', array( $post_grid_widget, 'set_columns' ) );
+		add_filter( 'generate_post_author', array( $post_grid_widget, 'enable_author' ) );
+		add_filter( 'post_class', array( $post_grid_widget, 'generate_blog_post_classes' ) );
+
+	}
+
+	/**
+	 * Init Controls
+	 *
+	 * Include controls files and register them
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function init_controls() {
+
+		// Include Control files.
+		// require_once __DIR__ . '/controls/test-control.php';
+
+		// Register control.
+	}
+
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have Elementor installed or activated.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_missing_main_plugin() {
+
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
+
+		$message = sprintf(
+			/* translators: 1: Plugin name 2: Elementor */
+			esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'elementor-test-extension' ),
+			'<strong>' . esc_html__( 'Frocentric Elementor Extension', 'elementor-test-extension' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'elementor-test-extension' ) . '</strong>'
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have a minimum required Elementor version.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_minimum_elementor_version() {
+
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
+
+		$message = sprintf(
+			/* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementor-test-extension' ),
+			'<strong>' . esc_html__( 'Frocentric Elementor Extension', 'elementor-test-extension' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'elementor-test-extension' ) . '</strong>',
+			 self::MINIMUM_ELEMENTOR_VERSION
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have a minimum required PHP version.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_minimum_php_version() {
+
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
+
+		$message = sprintf(
+			/* translators: 1: Plugin name 2: PHP 3: Required PHP version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementor-test-extension' ),
+			'<strong>' . esc_html__( 'Frocentric Elementor Extension', 'elementor-test-extension' ) . '</strong>',
+			'<strong>' . esc_html__( 'PHP', 'elementor-test-extension' ) . '</strong>',
+			 self::MINIMUM_PHP_VERSION
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+}
+
+Frocentric_Elementor_Extension::instance();
