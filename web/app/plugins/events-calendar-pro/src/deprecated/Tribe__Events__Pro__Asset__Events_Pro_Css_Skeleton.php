b532d6705453c75c1d921af32998938d2ac65268
Genyus <genyus@gmail.com>
2021-04-27 02:45:13 +0100

    chore(deps): remove manually installed plugins
    Removes Events Calendar Pro, Community Events and Filterbar plugins

--- web/app/plugins/events-calendar-pro/src/deprecated/Tribe__Events__Pro__Asset__Events_Pro_Css_Skeleton.php
+++
@@ -1,9 +0,0 @@
-<?php
-_deprecated_file( __FILE__, '4.4.30', 'Deprecated class in favor of using `tribe_asset` registration' );
-
-class Tribe__Events__Pro__Asset__Events_Pro_Css_Skeleton extends Tribe__Events__Asset__Abstract_Events_Css {
-
-	public function handle( array &$stylesheets, $mobile_break ) {
-		$stylesheets['tribe-events-calendar-pro-style'] = 'tribe-events-pro-skeleton.css';
-	}
-}
