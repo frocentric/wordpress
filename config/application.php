fc801aa212a93b6f2382c3afe2108bf88de26042
Genyus <genyus@gmail.com>
2021-08-19 18:12:23 +0100

    fix: restructure OneAll configuration loading
    Restructure configuration to correctly support remote environments

--- config/application.php
+++ config/application.php
@@ -206,6 +206,13 @@ Config::define( 'WPMS_GMAIL_CLIENT_SECRET', env( 'WPMS_GMAIL_CLIENT_SECRET' ) );
 Config::define( 'FEEDZY_ALLOW_UNSAFE_HTML', true );
 
 /**
+ * OneAll Settings
+ */
+Config::define( 'OA_SOCIAL_LOGIN_SETTINGS_API_KEY', env( 'OA_SOCIAL_LOGIN_SETTINGS_API_KEY' ) ?? '' );
+Config::define( 'OA_SOCIAL_LOGIN_SETTINGS_API_SECRET', env( 'OA_SOCIAL_LOGIN_SETTINGS_API_SECRET' ) ?? '' );
+Config::define( 'OA_SOCIAL_LOGIN_SETTINGS_SUBDOMAIN', env( 'OA_SOCIAL_LOGIN_SETTINGS_SUBDOMAIN' ) ?? '' );
+
+/**
  * Allow WordPress to detect HTTPS when used behind a reverse proxy or a load balancer
  * See https://codex.wordpress.org/Function_Reference/is_ssl#Notes
  */
