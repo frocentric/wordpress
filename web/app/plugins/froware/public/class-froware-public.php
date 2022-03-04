4469f213e6f252da15dd7e59cae8fe643aaa8ccb
Genyus <genyus@gmail.com>
2022-03-04 22:11:35 +0000

    fix: fixes Elementor landing page 404 errors
    Prevents 404 errors when custom permalinks are enabled
    See https://github.com/elementor/elementor/issues/16244

--- web/app/plugins/froware/public/class-froware-public.php
+++ web/app/plugins/froware/public/class-froware-public.php
@@ -243,6 +243,37 @@ class Froware_Public {
 	}
 
 	/**
+	 * Fixes landing page 404 when non-standard permalinks are enabled.
+	 *
+	 * @param \WP_Query $query
+	 * @return   string
+	 * @since    1.0.0
+	 */
+	public function elementor_pre_get_posts( \WP_Query $query ) {
+		if (
+			// If the post type includes the Elementor landing page CPT.
+			class_exists( '\Elementor\Modules\LandingPages\Module' )
+			&& is_array( $query->get( 'post_type' ) )
+			&& in_array( \Elementor\Modules\LandingPages\Module::CPT, $query->get( 'post_type' ), true )
+			// If custom permalinks are enabled.
+			&& '' !== get_option( 'permalink_structure' )
+			// If the query is for a front-end page.
+			&& ( ! is_admin() || wp_doing_ajax() )
+			&& $query->is_main_query()
+			// If the query is for a page.
+			&& isset( $query->query['page'] )
+			// If the query is not for a static home/blog page.
+			&& ! is_home()
+			// If the page name has been set and is not for a path.
+			&& ! empty( $query->query['pagename'] )
+			&& false === strpos( $query->query['pagename'], '/' )
+			// If the name has not already been set.
+			&& empty( $query->query['name'] ) ) {
+			$query->set( 'name', $query->query['pagename'] );
+		}
+	}
+
+	/**
 	 * Add categories and tags to pages
 	 *
 	 * @since    1.1.0
