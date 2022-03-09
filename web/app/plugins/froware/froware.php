1c4e83cc6718148888503a72f74ba8759a076528
Genyus <genyus@gmail.com>
2022-03-09 05:12:08 +0000

    fix: removes over-broad footer margin

--- web/app/plugins/froware/froware.php
+++ web/app/plugins/froware/froware.php
@@ -34,7 +34,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+define( 'PLUGIN_NAME_VERSION', '1.1.0' );
 
 if ( ! function_exists( 'activate_froware' ) ) {
 	/**
