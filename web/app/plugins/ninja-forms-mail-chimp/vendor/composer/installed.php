cede4ddd6955bc22e52b0c8cb7286cbdda529d09
Genyus <genyus@gmail.com>
2022-09-17 03:37:56 +0100

    fix: adds missing vendor directories

---
+++ web/app/plugins/ninja-forms-mail-chimp/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ninja-forms/ninja-forms-mailchimp',
+        'pretty_version' => '3.3.2',
+        'version' => '3.3.2.0',
+        'reference' => '158f601b9b963af4dffedd75819c2395e9f5ad24',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'ninja-forms/ninja-forms-mailchimp' => array(
+            'pretty_version' => '3.3.2',
+            'version' => '3.3.2.0',
+            'reference' => '158f601b9b963af4dffedd75819c2395e9f5ad24',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
