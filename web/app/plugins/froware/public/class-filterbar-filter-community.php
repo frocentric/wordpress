08496a5f54f4312616829190f3f89bb6b224f4a7
Genyus <genyus@gmail.com>
2022-09-25 03:52:53 +0100

    feat(events): adds custom taxonomy filters Enables filtering by community, discipline and interest

---
+++ web/app/plugins/froware/public/class-filterbar-filter-community.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Class Filterbar_Filter_Community
+ */
+class Filterbar_Filter_Community extends Filterbar_Filter_Taxonomy {
+	protected function get_taxonomy() {
+		return 'community';
+	}
+}
