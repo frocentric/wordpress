a3058d70f6e43a1cc12abadeef9853bfe7894be3
Genyus <genyus@gmail.com>
2022-09-28 17:08:44 +0100

    chore: change 'community' taxonomy to 'audience'

---
+++ web/app/plugins/froware/public/class-filterbar-filter-audience.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Class Filterbar_Filter_Audience
+ */
+class Filterbar_Filter_Audience extends Filterbar_Filter_Taxonomy {
+	protected function get_taxonomy() {
+		return 'audience';
+	}
+}
