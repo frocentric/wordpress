a2986c5e6bcb20fb3f99ac165fbc34cd3df28ea0
Gary McPherson <genyus@gmail.com>
2023-07-14 20:22:45 +0100

    fix: ensure citations are displayed for imported posts (#892)
    * fix: replace "read more" link with regex
    
    Use more flexible pattern matching than previous string replacement
    
    * refactor: extract citation replacement as function
    
    Prepare for switch from import action to content filter
    
    * feat: insert citation during render, not import
    
    Filters post content instead of hard-coding during import
    
    * style: add consistent whitespace to satisfy PHPCS
    
    Closes #891

--- web/app/plugins/frocentric/includes/Customizations/Feedzy.php
+++ web/app/plugins/frocentric/includes/Customizations/Feedzy.php
@@ -75,10 +75,29 @@ class Feedzy {
 	 */
 	public static function hooks() {
 		if ( Utils::is_request( Constants::FRONTEND_REQUEST ) ) {
+			add_action( 'feedzy_after_post_import', array( __CLASS__, 'save_item_title' ), 10, 3 );
 			add_filter( 'feedzy_content', array( __CLASS__, 'feedzy_content' ), 5, 2 );
 			add_filter( 'feedzy_insert_post_args', array( __CLASS__, 'feedzy_insert_post_args' ), 10, 6 );
 			add_filter( 'feedzy_item_filter', array( __CLASS__, 'feedzy_item_filter' ), 10, 5 );
+			add_filter( 'the_content', array( __CLASS__, 'set_post_citation' ) );
 		}
+	}
+
+	/**
+	 * Replaces the default "Read More" link to the source with a formatted citation
+	 */
+	protected static function replace_citation( $content, $author, $title, $url ) {
+		$pattern = '/<a\b[^>]*href=["\']' . preg_quote( $url, '/' ) . '["\'][^>]*class=["\']feedzy-rss-link-icon["\'][^>]*>' . __( 'Read More', 'feedzy-rss-feeds' ) . '<\/a>/i';
+		$citation = '<aside class="cite">' . __( 'Originally posted by ', 'frocentric' ) . $author . ' to <a href="' . $url . '" target="_blank" rel="nofollow">' . $title . '</a></aside>';
+
+		return preg_replace( $pattern, $citation, $content );
+	}
+
+	/**
+	 * Saves the original item title as metadata for the imported post
+	 */
+	public static function save_item_title( $post_id, $feedzy_item, $feedzy_settings ) {
+		update_post_meta( $post_id, 'feedzy_item_title', sanitize_text_field( $feedzy_item['item_title'] ) );
 	}
 
 	/**
@@ -105,8 +124,6 @@ class Feedzy {
 			}
 		}
 
-		$args = self::set_post_citation( $args, $item, $author );
-
 		return $args;
 	}
 
@@ -122,26 +139,28 @@ class Feedzy {
 	/**
 	 * Replaces the default "Read More" link to the source with a formatted citation
 	 */
-	private static function set_post_citation( $args, $item, $author ) {
-		$author_name = '';
+	public static function set_post_citation( $content ) {
+		global $post;
 
-		if ( ! empty( $author ) ) {
-			$author_name = $author->display_name;
-		} else {
-			if ( $item['item_author'] ) {
-				if ( is_string( $item['item_author'] ) ) {
-					$author_name = $item['item_author'];
-				} elseif ( is_object( $item['item_author'] ) ) {
-					$author_name = $item['item_author']->get_name();
-				}
+		if ( ! empty( $post ) && get_post_meta( $post->ID, 'feedzy', true ) ) {
+			$author = get_the_author_meta( 'display_name', intval( $post->post_author ) );
+			$title = get_post_meta( $post->ID, 'feedzy_item_title', true );
+			$url = get_post_meta( $post->ID, 'feedzy_item_url', true );
+
+			if ( empty( $author ) ) {
+				$author = get_post_meta( $post->ID, 'feedzy_item_author', true );
+			}
+
+			if ( empty( $title ) ) {
+				$title = $post->post_title;
+			}
+
+			if ( ! empty( $author ) && ! empty( $title ) && ! empty( $url ) ) {
+				$content = self::replace_citation( $content, $author, $title, $url );
 			}
 		}
 
-		$item_link  = '<a href="' . $item['item_url'] . '" target="_blank" class="feedzy-rss-link-icon">' . __( 'Read More', 'feedzy-rss-feeds' ) . '</a>';
-		$citation = '<aside class="cite">' . __( 'Originally posted by ', 'frocentric' ) . $author_name . ' to <a href="' . $item['item_url'] . '" target="_blank" rel="nofollow">' . $args['post_title'] . '</a></aside>';
-		$args['post_content'] = str_replace( $item_link, $citation, $args['post_content'] );
-
-		return $args;
+		return $content;
 	}
 
 	/**
