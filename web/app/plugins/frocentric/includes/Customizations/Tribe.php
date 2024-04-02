9bba71b4334d7eb174bae9a0eceb8e10c235c0f9
Genyus <genyus@gmail.com>
2024-04-01 20:56:25 -0400

    fix: improves markup cleaning logic

--- web/app/plugins/frocentric/includes/Customizations/Tribe.php
+++ web/app/plugins/frocentric/includes/Customizations/Tribe.php
@@ -176,11 +176,10 @@ class Tribe {
 		$xpath = new \DOMXPath( $dom );
 		$styled_divs = $xpath->query( '//div[@style]' );
 
-		for ( $count = 2; $count >= 0; $count-- ) {
-			$current_node = $styled_divs->item( $count );
+		foreach( $styled_divs as $current_node) {
 			$style = $current_node->attributes->getNamedItem( 'style' )->nodeValue;
 			// Strip tags from the styled <div> elements unless it's the image container
-			if ( $style === 'margin-top: 20px;' || strpos( $style, 'font-size:' ) !== false ) {
+			if ( $style === 'margin-top: 20px' || strpos( $style, 'font-size:' ) !== false ) {
 				strip_tags( $current_node );
 			}
 		}
