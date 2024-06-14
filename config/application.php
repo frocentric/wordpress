c3d2a1a0fa1004a34621fb355c9ef1020549a96a
Genyus <genyus@gmail.com>
2024-06-14 01:10:15 +0100

    revert: removes multisite configuration

--- config/application.php
+++ config/application.php
@@ -128,18 +128,6 @@ if ( env( 'FRONTEND_EXCLUDED_ERROR_LEVELS' ) ) {
 	Config::define( 'FRONTEND_EXCLUDED_ERROR_LEVELS', explode( ',', env( 'FRONTEND_EXCLUDED_ERROR_LEVELS' ) ) );
 }
 
-/**
- * Multisite Settings
- */
-Config::define( 'WP_ALLOW_MULTISITE', false );
-if ( ! defined( 'MULTISITE' ) ) {
-	Config::define( 'MULTISITE', false );
-}
-Config::define( 'SUBDOMAIN_INSTALL', env( 'SUBDOMAIN_INSTALL' ) ?? true );
-Config::define( 'DOMAIN_CURRENT_SITE', env( 'DOMAIN_CURRENT_SITE' ) );
-Config::define( 'PATH_CURRENT_SITE', env( 'PATH_CURRENT_SITE' ) ?? '/' );
-Config::define( 'SITE_ID_CURRENT_SITE', env( 'SITE_ID_CURRENT_SITE' ) ?? 1 );
-Config::define( 'BLOG_ID_CURRENT_SITE', env( 'BLOG_ID_CURRENT_SITE' ) ?? 1 );
 Config::define( 'WP_DEFAULT_THEME', 'frocentric');
 if ( ! defined( 'WP_CLI' ) ) {
 	Config::define( 'COOKIE_DOMAIN', '.' . env( 'DOMAIN_CURRENT_SITE' ) ?? 'frocentric.io' ); // phpcs:ignore
